9305c481ccdd526ac38d2cbe8170d2db1594129a
TTaiJin <taeparis980110@gmail.com>
2024-12-01 22:10:34 +0900

    f3 b

--- src/test/java/com/ll/standard/util/UtilTest.java
+++ src/test/java/com/ll/standard/util/UtilTest.java
@@ -11,33 +11,37 @@ public class UtilTest {
     @DisplayName("파일을 생성할 수 있다.")
     public void t1() {
         // given
-        String filePath = "test.txt";
+        String filePath = "temp/test.txt";
 
         // when
         Util.file.touch(filePath);
 
         // then
         assertThat(Util.file.exists(filePath)).isTrue();
+
+        Util.file.delete(filePath);
     }
 
     @Test
     @DisplayName("파일의 내용을 수정할 수 있고, 읽을 수 있다.")
     public void t2() {
         // given
-        String filePath = "test.txt";
+        String filePath = "temp/test.txt";
 
         // when
         Util.file.set(filePath, "내용");
 
         // then
         assertThat(Util.file.get(filePath, "")).isEqualTo("내용");
+
+        Util.file.delete(filePath);
     }
 
     @Test
     @DisplayName("파일을 삭제할 수 있다.")
     public void t3() {
         // given
-        String filePath = "test.txt";
+        String filePath = "temp/test.txt";
 
         // when
         Util.file.touch(filePath);
