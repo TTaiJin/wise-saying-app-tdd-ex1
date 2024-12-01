717c79ff437f1391740a9806b73698d08724ebe6
TTaiJin <taeparis980110@gmail.com>
2024-12-01 19:12:34 +0900

    f2 r

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingController.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingController.java
@@ -14,4 +14,16 @@ public class WiseSayingController {
 
         assertThat(output).contains("== 명언 앱 ==");
     }
+
+    @Test
+    @DisplayName("명령) 등록")
+    public void t2() {
+       String output = AppTest.run("""
+               등록
+               현재를 사랑하라.
+               작자미상
+               """.stripIndent().trim());
+        assertThat(output).contains("명언 : ")
+                .contains("작가 : ");
+    }
 }
