7f861fd7515058f1a2a885c00710b7be6f1085cd
TTaiJin <taeparis980110@gmail.com>
2024-12-01 23:55:33 +0900

    f1 g

--- src/test/java/com/ll/standard/util/JsonUtilTest.java
+++ src/test/java/com/ll/standard/util/JsonUtilTest.java
@@ -13,7 +13,7 @@ public class JsonUtilTest {
     @DisplayName("맵을 JSON으로 바꿀 수 있다.")
     public void t1() {
         // given
-        Map<String, Object>map = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
         map.put("name", "이름");
 
         // when
@@ -24,6 +24,6 @@ public class JsonUtilTest {
                 {
                     "name": "이름"
                 }
-                """.stripIndent());
+                """.stripIndent().trim());
     }
 }
