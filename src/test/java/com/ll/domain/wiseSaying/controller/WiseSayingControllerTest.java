eb1661900560401cac8f65f62a4cac5a96bb75e1
TTaiJin <taeparis980110@gmail.com>
2024-12-01 20:47:13 +0900

    f6 r

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -69,4 +69,20 @@ public class WiseSayingControllerTest {
                 .contains("2 / 작자미상 / 과거에 집착하지 마라.")
                 .contains("1 / 작자미상 / 현재를 사랑하라.");
     }
+
+    @Test
+    @DisplayName("삭제를 입력하면 명언이 삭제된다.")
+    public void t6() {
+        String output = AppTest.run("""
+               등록
+               현재를 사랑하라.
+               작자미상
+               등록
+               과거에 집착하지 마라.
+               작자미상
+               목록
+               삭제?id=1
+               """);
+        assertThat(output).contains("1번 명언이 삭제되었습니다.");
+    }
 }
